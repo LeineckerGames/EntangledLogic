c027fa95f71f906d6b6775af9bc9a3b8ce9e204f
HamsterRunner67 <njlamon@gmail.com>
2026-06-06 13:32:46 -0400

    Built Pawn Blueprint and added it the gamemode for use

---
+++ Source/EntangledLogic/Core/FactoryGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FactoryGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENTANGLEDLOGIC_API AFactoryGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
