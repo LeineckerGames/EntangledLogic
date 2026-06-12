5e51b6fe01f9e48faad7d90a001ca7c58cf5767c
HamsterRunner67 <njlamon@gmail.com>
2026-06-12 16:59:01 -0400

    Added 5x5 Larger Test Factory
    Just using the to make sure all collision placement and deletion works, especially on larger factories.

---
+++ Source/EntangledLogic/Objects/Factories/LargerTestFactory.cpp
@@ -0,0 +1,2 @@
+#include "LargerTestFactory.h"
+
---
+++ Source/EntangledLogic/Objects/Factories/LargerTestFactory.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FactoryBase.h"
+#include "LargerTestFactory.generated.h"
+
+UCLASS()
+class ENTANGLEDLOGIC_API ALargerTestFactory : public AFactoryBase
+{
+	GENERATED_BODY()
+	
+};
