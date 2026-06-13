dfa6dad2e1c07c290c4144a62d1563d02a06366b
HamsterRunner67 <njlamon@gmail.com>
2026-06-13 03:34:53 -0400

    Creates Factory Classes & Blueprints + X Factory Model

---
+++ Source/EntangledLogic/Objects/Factories/Gates/YGateFactory.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../FactoryBase.h"
+#include "YGateFactory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENTANGLEDLOGIC_API AYGateFactory : public AFactoryBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/EntangledLogic/Objects/Factories/Gates/ZGateFactory.cpp
@@ -0,0 +1,5 @@
+
+
+
+#include "ZGateFactory.h"
+
