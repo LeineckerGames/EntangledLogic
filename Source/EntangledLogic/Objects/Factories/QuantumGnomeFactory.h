578c703160d94f9113e45796b9739559bce9fb4f
HamsterRunner67 <njlamon@gmail.com>
2026-06-14 15:50:02 -0400

    Quantum Gnome.

---
+++ Source/EntangledLogic/Objects/Factories/QuantumGnomeFactory.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FactoryBase.h"
+#include "QuantumGnomeFactory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENTANGLEDLOGIC_API AQuantumGnomeFactory : public AFactoryBase
+{
+	GENERATED_BODY()
+	
+};
