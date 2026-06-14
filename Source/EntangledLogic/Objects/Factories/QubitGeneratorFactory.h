2791d6df169c842893d1ddd35c886ece9671b1d3
HamsterRunner67 <njlamon@gmail.com>
2026-06-13 23:29:37 -0400

    Added Qubit Generator Class / Blueprint

---
+++ Source/EntangledLogic/Objects/Factories/QubitGeneratorFactory.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FactoryBase.h"
+#include "QubitGeneratorFactory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENTANGLEDLOGIC_API AQubitGeneratorFactory : public AFactoryBase
+{
+	GENERATED_BODY()
+	
+};
