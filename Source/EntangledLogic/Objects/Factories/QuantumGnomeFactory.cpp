b380b761b25506904f6380ee2331543b1d7b9f40
HamsterRunner67 <njlamon@gmail.com>
2026-06-15 00:30:08 -0400

    Gnomes now explode

--- Source/EntangledLogic/Objects/Factories/QuantumGnomeFactory.cpp
+++ Source/EntangledLogic/Objects/Factories/QuantumGnomeFactory.cpp
@@ -1,5 +1,60 @@
-
-
-
 #include "QuantumGnomeFactory.h"
+#include "Components/AudioComponent.h"
+#include "GeometryCollection/GeometryCollectionComponent.h"
+#include "EntangledLogic/Core/Subsystems/GridPlacementSubsystem.h"
+#include "EntangledLogic/Core/Components/GridPlacementComponent.h"
 
+AQuantumGnomeFactory::AQuantumGnomeFactory()
+{
+	AudioComponenet = CreateDefaultSubobject<UAudioComponent>(TEXT("AudioComponenet"));
+	AudioComponenet->SetupAttachment(RootComponent);
+
+	GnomeFracturedGeometry = CreateDefaultSubobject<UGeometryCollectionComponent>(TEXT("GnomeFracturedGeometry"));
+	GnomeFracturedGeometry->SetupAttachment(FactoryMesh);
+}
+
+void AQuantumGnomeFactory::BeginPlay()
+{
+	Super::BeginPlay();
+	if (GnomeFracturedGeometry)
+	{
+		GnomeFracturedGeometry->SetVisibility(false);
+		GnomeFracturedGeometry->SetSimulatePhysics(false);
+	}
+}
+
+void AQuantumGnomeFactory::Explode()
+{
+	if (GnomeFracturedGeometry)
+	{
+		GnomeFracturedGeometry->SetVisibility(true);
+		FactoryMesh->SetVisibility(false);
+		GnomeFracturedGeometry->SetSimulatePhysics(true);
+		GnomeFracturedGeometry->ApplyExternalStrain(0, FVector(0.0f, 25.0f, 0.0f), 200.0f, 5000.0f);
+	}
+}
+
+void AQuantumGnomeFactory::Interact(EPlacementMode PlacementMode)
+{
+	Super::Interact(PlacementMode);
+	if (PlacementMode == EPlacementMode::Disabled)
+	{
+		ClickCount++;
+		if (ClickCount >= 10)
+		{
+			// Make gnome explode
+			Explode();
+			UGridPlacementSubsystem* GridPlacement = GetWorld()->GetSubsystem<UGridPlacementSubsystem>();
+			TArray<FGridCoordinate> GridLocations = GridPlacement->GridComponentToCoordinates(GridPlacementComponent);
+			GridPlacement->SetPlacedPositionMap(GridLocations, GridPlacementComponent->GetFactoryShape(), false);
+			SetLifeSpan(5.0f);
+		}
+		else
+		{
+			AudioComponenet->SetPitchMultiplier(ClickCount / 5);
+			AudioComponenet->Play();
+		}
+	}
+
+
+}
--- Source/EntangledLogic/Objects/Factories/QuantumGnomeFactory.h
+++ Source/EntangledLogic/Objects/Factories/QuantumGnomeFactory.h
@@ -1,17 +1,31 @@
-
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "FactoryBase.h"
 #include "QuantumGnomeFactory.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class ENTANGLEDLOGIC_API AQuantumGnomeFactory : public AFactoryBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	AQuantumGnomeFactory();
+
+	virtual void Interact(EPlacementMode PlacementMode) override;
+
+protected:
+
+	virtual void BeginPlay() override;
+
+	void Explode();
+
+	int32 ClickCount = 0;
+
+	UPROPERTY(VisibleAnywhere)
+	class UAudioComponent* AudioComponenet;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	class UGeometryCollectionComponent* GnomeFracturedGeometry;
+
 };
