e7a90bdbcda3bb1f07713e08dd1f78250c9d4598
HamsterRunner67 <njlamon@gmail.com>
2026-06-20 03:06:19 -0400

    Create the Saving and Loading Class

---
+++ Source/EntangledLogic/Core/Subsystems/SavingLoadingSubsystem.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "SavingLoadingSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENTANGLEDLOGIC_API USavingLoadingSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
