8a4957feae8aba8a9e409358adfae5e23b66312c
HamsterRunner67 <njlamon@gmail.com>
2026-07-01 16:30:15 -0400

    Created Black Hole Factory(Trash Can)
    doesnt do anything yet but setup the factory and the material so far.

---
+++ Source/EntangledLogic/Objects/Factories/BlackholeFactory.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FactoryBase.h"
+#include "BlackholeFactory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENTANGLEDLOGIC_API ABlackholeFactory : public AFactoryBase
+{
+	GENERATED_BODY()
+	
+};
