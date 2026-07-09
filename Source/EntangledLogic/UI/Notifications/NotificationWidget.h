377030a2478586a5d63e955cc390f89360d38ea2
cygnus1729 <ma404119@ucf.edu>
2026-07-09 16:19:29 -0400

    Disable notification widget temporarily

--- Source/EntangledLogic/UI/Notifications/NotificationWidget.h
+++ Source/EntangledLogic/UI/Notifications/NotificationWidget.h
@@ -19,8 +19,8 @@ protected:
 	UPROPERTY(meta = (BindWidget))
 	class USizeBox* NotifcationSizeBox;
 
-	UPROPERTY(meta = (BindWidget))
-	class UTextBox* NotificationText;
+	//UPROPERTY(meta = (BindWidget))
+	//class UTextBox* NotificationText;
 
 	UPROPERTY(meta = (BindWidget))
 	class UProgressBar* NotificationText;
