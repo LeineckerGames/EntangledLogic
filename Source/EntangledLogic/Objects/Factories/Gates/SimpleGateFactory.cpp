678914d3af07bc5fef2821a3c01f645d46135ef1
cygnus1729 <ma404119@ucf.edu>
2026-07-22 19:56:43 -0400

    Added S and T gates

---
+++ Source/EntangledLogic/Objects/Factories/Gates/SimpleGateFactory.cpp
@@ -0,0 +1,121 @@
+
+#include "SimpleGateFactory.h"
+#include "Components/SplineComponent.h"
+#include "EntangledLogic/Objects/Factories/Components/FactoryInputComponent.h"
+#include "EntangledLogic/Objects/Factories/Components/FactoryOutputComponent.h"
+#include "EntangledLogic/Interfaces/InputOutputInterface.h"
+#include "EntangledLogic/Core/Subsystems/GridPlacementSubsystem.h"
+#include "EntangledLogic/Core/Subsystems/QubitDataSubsystem.h"
+#include "EntangledLogic/Core/Framework/QuantumGatesEnum.h"
+#include "Components/SceneCaptureComponent2D.h"
+
+ASimpleGateFactory::ASimpleGateFactory()
+{
+	// Create Qubit movement spline
+	QubitSplines.Add(CreateDefaultSubobject<USplineComponent>(TEXT("QubitSpline0")));
+	QubitSplines[0]->SetupAttachment(FactoryMesh);
+}
+
+// Need to add the deletion of qubits once they transfer
+
+void ASimpleGateFactory::OnFactoryTick()
+{
+	Super::OnFactoryTick();
+	if (IsQubitProcessed)
+	{
+		bool IsSuccess = OutputQubits();
+		if (IsSuccess)
+		{
+			UE_LOG(LogTemp, Display, TEXT("Outputting Qubits, IsQubitProcessed = false"))
+				IsQubitProcessed = false;
+		}
+	}
+}
+
+void ASimpleGateFactory::OnQubitProcessed()
+{
+	Super::OnQubitProcessed();
+	UWorld* world = GetWorld();
+	if (world)
+	{
+		UQubitDataSubsystem* QubitSubsystem = GetWorld()->GetSubsystem<UQubitDataSubsystem>();
+		if (QubitSubsystem)
+		{
+			//UE_LOG(LogTemp, Display, TEXT("Applying the X Gate on the qubit"))
+			QubitSubsystem->Apply(*Qubits[0], Gate);
+			CurrentSplineMode = QubitSplineMode::EXIT_MODE;
+			UpdateQubitDisplay();
+		}
+	}
+}
+
+void ASimpleGateFactory::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	if (EndPlayReason == EEndPlayReason::Destroyed)
+	{
+		// Get the current actor pointers
+		AActor* InputSlot0Actor = InputComponents[0]->InputSlot;
+		AActor* OutputSlot0Actor = OutputComponents[0]->OutputSlot;
+
+
+		// Update the prev and next factory pointers
+		if (InputSlot0Actor)
+		{
+			IInputOutputInterface* InputSlotActorInputOutputInterface = Cast<IInputOutputInterface>(InputSlot0Actor);
+			if (InputSlotActorInputOutputInterface)
+			{
+				UE_LOG(LogTemp, Display, TEXT("Connect All Outputs Running in destroyed"));
+				// Updates the previous factory to connect to the current
+				InputSlotActorInputOutputInterface->ConnectAllOutputs();
+			}
+		}
+
+		if (OutputSlot0Actor)
+		{
+			IInputOutputInterface* OutputSlotActorInputOutputInterface = Cast<IInputOutputInterface>(OutputSlot0Actor);
+			if (OutputSlotActorInputOutputInterface)
+			{
+				UE_LOG(LogTemp, Display, TEXT("Connect All Inputs Running in destroyed"));
+				// Updates the next factory to connect to the current
+				OutputSlotActorInputOutputInterface->ConnectAllInputs();
+			}
+		}
+	}
+	Super::EndPlay(EndPlayReason);
+}
+
+// Input Output Interface
+void ASimpleGateFactory::ConnectAllInputsAndOutputs()
+{
+	Super::ConnectAllInputsAndOutputs();
+
+	for (UFactoryInputComponent* CurrentInputComponent : InputComponents)
+	{
+		ConnectInputComponent(CurrentInputComponent, true);
+	}
+
+	for (UFactoryOutputComponent* CurrentOutputComponent : OutputComponents)
+	{
+		ConnectOutputComponent(CurrentOutputComponent, true);
+	}
+}
+
+void ASimpleGateFactory::ConnectAllInputs()
+{
+	Super::ConnectAllInputs();
+
+	for (UFactoryInputComponent* CurrentInputComponent : InputComponents)
+	{
+		ConnectInputComponent(CurrentInputComponent, false);
+	}
+}
+
+void ASimpleGateFactory::ConnectAllOutputs()
+{
+	Super::ConnectAllOutputs();
+
+	for (UFactoryOutputComponent* CurrentOutputComponent : OutputComponents)
+	{
+		ConnectOutputComponent(CurrentOutputComponent, false);
+	}
+}
